c5f490120332edd7cc55d9b5963c0789fe464cef
sweemeng <sweester@gmail.com>
2018-02-05 14:30:54 +0800

    Make entry to router for organization

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Person from '@/components/Person'
 import PersonEdit from '@/components/PersonEdit'
 import Login from '@/components/Login'
 import Logout from '@/components/Logout'
+import Organization from '@/components/Organization'
+import OrganizationEdit from '@/components/OrganizationEdit'
 
 Vue.use(Router)
 
@@ -34,6 +36,16 @@ export default new Router({
       path: '/logout',
       name: 'Logout',
       component: Logout
+    },
+    {
+      path: '/organizations/create',
+      name: 'Organization',
+      component: Organization
+    },
+    {
+      path: '/organizations/edit/:entity_id',
+      name: 'OrganizationEdit',
+      component: OrganizationEdit
     }
   ]
 })
