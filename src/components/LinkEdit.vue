587eb4c57585de1b5967e796dec27c24bd071c4a
sweemeng <sweester@gmail.com>
2018-02-12 14:50:21 +0800

    Add link edit component

---
+++ src/components/LinkEdit.vue
@@ -0,0 +1,111 @@
+<template>
+  <el-card class="form">
+    <form-schema ref="formSchema" :schema="schema" v-model="model">
+      <el-button type="primary" @click="submit">Edit</el-button>
+    </form-schema>
+  </el-card>
+</template>
+<script>
+import FormSchema from 'vue-json-schema'
+import schema from '../schema/link_form.json'
+import { HTTP } from '../http-common.js'
+
+FormSchema.setComponent('form', 'el-form', ({ vm }) => {
+  const labelPosition = 'top'
+  const labelWidth = '120px'
+  const model = vm.data
+  const rule = {}
+
+  vm.fields.forEach((field) => {
+    const type = field.schemaType
+    const required = field.required
+    const message = field.title
+    const trigger = 'blur'
+
+    rule[field.name] = { type, required, message, trigger }
+  })
+
+  return { labelWidth, rule, model, labelPosition }
+})
+
+FormSchema.setComponent('label', 'el-form-item', ({ field }) => ({
+  prop: field.title
+}))
+
+FormSchema.setComponent('text', 'el-input')
+
+export default {
+  data () {
+    return {
+      schema: schema,
+      model: {}
+    }
+  },
+  watch: {
+    '$route': function (to, from) {
+      var entity = this.$route.params.entity
+      var entityId = this.$route.params.entity_id
+      this.fetch_entity(entity, entityId)
+    }
+  },
+  created () {
+    console.log(this.$store.state.loggedIn)
+    var entity = this.$route.params.entity
+    var entityId = this.$route.params.entity_id
+    this.fetch_entity(entity, entityId)
+  },
+  methods: {
+    submit (e) {
+      var entity = this.$route.params.entity
+      var entityId = this.$route.params.entity_id
+      var url = '/en/'+ entity + '/' + entityId
+      var loggedIn = this.$store.state.loggedIn
+      console.log(loggedIn)
+      if (loggedIn) {
+        this.$refs.formSchema.form().validate((valid) => {
+          if (valid) {
+            console.log(JSON.stringify(this.model))
+            this.$refs.formSchema.clearErrorMessage()
+            HTTP.put(url, this.model)
+              .then(response => {
+                console.log(response)
+              })
+              .catch(e => {
+                console.log(e)
+              })
+          } else {
+            this.$refs.formSchema.setErrorMessage('Please Fill in the form')
+            return false
+          }
+        })
+      } else {
+        this.$router.push('/login')
+      }
+    },
+    fetch_entity (entity, entityId) {
+      var url = '/en/' + entity + '/' + entityId
+      HTTP.get(url)
+        .then(response => {
+          this.model = response.data.result
+          console.log(this.model)
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    }
+  },
+  components: {
+    FormSchema
+  }
+}
+</script>
+<style scoped>
+  .form {
+    text-align: left;
+    width: 600px;
+    margin: auto;
+  }
+  .el-form {
+    text-align: left;
+  }
+</style>
