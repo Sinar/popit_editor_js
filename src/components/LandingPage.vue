59a50141754b7026738c1c0b253d55ace56611bd
sweeneng <sweester@gmail.com>
2018-03-05 21:09:41 +0800

    initial implementation of landing page

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+     <ul>
+       <li><a href="/persons/list">Persons</a></li>
+       <li><a href="/organizations/list">Organizations</a></li>
+       <li><a href="/memberships/list">Memberships</a></li>
+       <li><a href="/posts/list">Post</a></li>
+     </ul>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
