ab6ef786b56739a63f348d037ca12fe7ae8eb731
sweemeng <sweester@gmail.com>
2018-03-05 14:30:41 +0800

    Add language changer, make sure this doesn't break everything else

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,53 @@
 <template>
-  <div id="app">
+  <div>
+    <div id="app">
     <img src="https://sinarproject.org/logo.png">
+    </div>
+    <div id="language">
+    <el-dropdown @command="handleCommand">
+      <el-button>
+        {{ languageName }}<i class="el-icon-arrow-down el-icon--right"></i>
+      </el-button>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item command="ms">Malay</el-dropdown-item>
+        <el-dropdown-item command="en">English</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: function () {
+    return {
+      language: 'en',
+      languageName: 'English',
+      languageKey: {
+        en: 'English',
+        ms: 'Malay'
+      }
+    }
+  },
+  created: function () {
+    var language = localStorage.getItem('language')
+    if (language === null) {
+      this.language = 'en'
+      this.languageName = 'English'
+    } else {
+      this.language = language
+      this.languageName = this.languageKey[language]
+    }
+  },
+  methods: {
+    handleCommand: function (command) {
+      localStorage.setItem('language', command)
+      this.language = command
+      this.languageName = this.languageKey[command]
+    }
+  }
 }
 </script>
 
@@ -19,5 +59,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#language {
+  text-align: right;
 }
 </style>
