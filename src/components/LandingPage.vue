29d526ec94835c05a7783afc81646d353a0741d3
sweeneng <sweester@gmail.com>
2018-03-06 09:09:11 +0800

    Just add some link and css to landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
+    <h1>This is the editor for Popit, Sinar Project's people and organization Data service</h1>
+    <h2>To get started, start with the links below</h2>
      <ul>
        <li><a href="#/persons/list">Persons</a></li>
        <li><a href="#/organizations/list">Organizations</a></li>
        <li><a href="#/memberships/list">Memberships</a></li>
        <li><a href="#/posts/list">Post</a></li>
+     </ul>
+     <br>
+     <h2>To help improve the editor go to the links below</h2>
+     <ul>
+       <li><a href="https://github.com/Sinar/popit_editor_js/issues">github page</a></li>
      </ul>
   </div>
 </template>
@@ -12,3 +19,19 @@
 export default {
 }
 </script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
