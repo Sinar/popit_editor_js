8ab6127a204ff42b0894bf2f590283c8e05488a2
sweeneng <sweester@gmail.com>
2018-03-06 08:56:51 +0800

    Add the right link to each entities

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
      <ul>
-       <li><a href="/persons/list">Persons</a></li>
-       <li><a href="/organizations/list">Organizations</a></li>
-       <li><a href="/memberships/list">Memberships</a></li>
-       <li><a href="/posts/list">Post</a></li>
+       <li><a href="#/persons/list">Persons</a></li>
+       <li><a href="#/organizations/list">Organizations</a></li>
+       <li><a href="#/memberships/list">Memberships</a></li>
+       <li><a href="#/posts/list">Post</a></li>
      </ul>
   </div>
 </template>
