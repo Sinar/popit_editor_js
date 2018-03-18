3c686f546a747cbcbf2e47438805f067056788a0
artze <majathurup28@gmail.com>
2018-03-18 14:31:37 +0800

    Switch axios baseURL between dev and production mode

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
 
 // make sure check for token in localStorage before use
+
+var baseURLConfig
+
+if (process.env.NODE_ENV === 'development') {
+  baseURLConfig = 'http://testing.popit.sinarproject.org'
+} else if (process.env.NODE_ENV === 'production') {
+  baseURLConfig = 'https://api.popit.sinarproject.org'
+}
+
 export const HTTP = axios.create({
-  baseURL: 'https://api.popit.sinarproject.org',
+  baseURL: baseURLConfig,
   headers: {
     Authorization: 'Token ' + localStorage.getItem('token')
   }
